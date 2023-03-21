15111e8286e56c158cb0941adf236062ef8c2464
jimba tshetrim <jtshetrim@gmail.com>
2023-03-21 17:48:08 +0600

    feat: added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import greekSalad from './assets/greek salad.jpg';
 import deliveryIcon from './assets/Dish icon.svg'
 import footerImg1 from './assets/Mario and Adrian A.jpg'
 import footerImg2 from './assets/Mario and Adrian b.jpg'
+import footerLogo from './assets/footer-logo.png'
 import './App.css';
 
 function App() {
@@ -71,6 +72,28 @@ function App() {
                 </section>
             </main>
             <footer>
+                <img src={footerLogo} alt="logo"/>
+                <div className="navigation">
+                    <h3>Navigation</h3>
+                        {
+                            ['Home', 'About', 'Menu', 'Reservations', 'Order Online', 'Login'].map(
+                                menu => <p>{menu}</p>
+                            )
+                        }
+                </div>
+                <div className="contact">
+                   <h3>Contact</h3>
+                    <p>3241 Maldova Way</p>
+                    <p>Chicago Illinois</p>
+                    <p>(123)-312-3133</p>
+                    <p>info@littlelemon.com</p>
+                </div>
+                <div className="socials">
+                    <h3>Connect</h3>
+                    <p>Facebook</p>
+                    <p>Twitter</p>
+                    <p>Instagram</p>
+                </div>
             </footer>
         </div>
     );
