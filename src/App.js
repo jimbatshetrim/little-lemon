575b0917b0bb2f2fec9f475b1ddfc703b6db769e
jimba tshetrim <jtshetrim@gmail.com>
2023-03-21 17:29:13 +0600

    feat: added company info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './assets/Logo.svg';
 import heroImg from './assets/restauranfood.jpg';
 import greekSalad from './assets/greek salad.jpg';
 import deliveryIcon from './assets/Dish icon.svg'
+import footerImg1 from './assets/Mario and Adrian A.jpg'
+import footerImg2 from './assets/Mario and Adrian b.jpg'
 import './App.css';
 
 function App() {
@@ -54,17 +56,22 @@ function App() {
                         }
                     </div>
                 </section>
-                <section className="testimonials">
-                    {
-                        ['' , '', '', '', '', '', '', '', ''].map(() =>
-                            <div className="testimonials-card">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                                <sub> - fdjsal</sub>
-                            </div>
-                        )
-                    }
+                <section className="company-info">
+                    <div className="company-description">
+                        <h2>Little Lemon</h2>
+                        <h5>Chicago</h5>
+                        <p>
+                            Little Lemon opened in 1995 by two italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later.
+                        </p>
+                    </div>
+                    <div className="company-img">
+                        <img src={footerImg1} alt="company1"/>
+                        <img alt="company2" id="secondImg" src={footerImg2}/>
+                    </div>
                 </section>
             </main>
+            <footer>
+            </footer>
         </div>
     );
 }
