fdb3eb170094e7b4f4c137cb0930a719daadb706
jimba tshetrim <jtshetrim@gmail.com>
2023-03-21 17:54:40 +0600

    feat: img overlap

--- src/App.js
+++ src/App.js
@@ -60,14 +60,14 @@ function App() {
                 <section className="company-info">
                     <div className="company-description">
                         <h2>Little Lemon</h2>
-                        <h5>Chicago</h5>
+                        <h3>Chicago</h3>
                         <p>
                             Little Lemon opened in 1995 by two italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later.
                         </p>
                     </div>
                     <div className="company-img">
-                        <img src={footerImg1} alt="company1"/>
-                        <img alt="company2" id="secondImg" src={footerImg2}/>
+                        <img src={footerImg1} id="overlapImg" alt="company1"/>
+                        <img alt="company2"  src={footerImg2}/>
                     </div>
                 </section>
             </main>
